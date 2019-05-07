671ff0f92146e270de3478d339de3a0b6827930d
Jelena Grimshaw <muchstudio@hotmail.com>
2019-05-07 19:20:32 +0000

    add get routes to /urls.json and /hello

--- express-server.js
+++ express-server.js
@@ -11,7 +11,13 @@ app.get('/', (req, res) => {
   res.send('Hello!');
 });
 
+app.get('/urls.json', (req, res) => {
+  res.json(urlDatabase);
+});
 
+app.get("/hello", (req, res) => {
+  res.send('<html><body>Hello <b>World</b></body></html>\n');
+});
 
 
 
